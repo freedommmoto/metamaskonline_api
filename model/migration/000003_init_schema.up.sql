967430b54e02f4c52e04fdcee0b21154d6b4d4a0
Patara.m <patara.m@cartrack.com>
2022-07-18 00:46:53 +0700

    add trigger and function for make pg_notify name code_confirm

---
+++ model/migration/000003_init_schema.up.sql
@@ -0,0 +1,15 @@
+create
+or replace function fn_code_stage_modified() returns trigger as $psql$
+begin
+    if NEW.deleted > (now() - INTERVAL '1 minute'):: timestamp with time zone then
+    perform pg_notify(
+            'code_confirm',NEW.id_user::varchar
+        );
+return new;
+end if;
+end;$psql$ language plpgsql;
+
+create trigger code_stage before
+    update
+    on line_owner_validation for each row
+    execute procedure fn_code_stage_modified();
